1b13af3472ae42d7142eae5d81542f1fbe4c8a72
Ricky-Castro <58087833+Ricky-Castro@users.noreply.github.com>
2019-12-20 17:46:32 -0300

    finalizado a estrutura em html. Pronto para estilização no css.

--- index.php
+++ index.php
@@ -10,40 +10,100 @@
 <body>
 
 <header>
-	<div class="Logo">LogoMarca</div><!--Logo-->
-	<nav class="desktop">
-		<ul>
-			<li><a href="">Home</a></li>
-			<li><a href="">Sobre</a></li>
-			<li><a href="">Serviços</a></li>
-			<li><a href="">Contato</a></li>
-		</ul>
-	</nav>
-	<nav class="mobile">
-	<ul>
-		<li><a href="">Home</a></li>
-		<li><a href="">Sobre</a></li>
-		<li><a href="">Serviços</a></li>
-		<li><a href="">Contato</a></li>
-	</ul>
-	</nav>
+	<div class="center">
+		<div class="Logo">LogoMarca</div><!--Logo-->
+		<nav class="desktop">
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Serviços</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+		</nav>
+		<nav class="mobile">
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Serviços</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+		</nav>
+	</div><!--center-->
 </header>
-<section class="banne-principal">
-	<form>
-		<h2>Qual o seu melhor e-mail?</h2>
-		<input type="email" name="email" required />
-		<input type="submit" name="acao" value="Cadastrar!" />
-	</form>
-</section><!--banner-principal-->
-<section class="descricao-autor">
-	<div class="w50">
-		<h2>Henrique de Castro</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet lorem non leo feugiat pulvinar. Nunc commodo posuere arcu, ut consequat ante viverra eu. Integer risus est, sodales sed urna et, pulvinar aliquam sem. Donec mi sem, iaculis eu augue vel, ultricies pretium quam. Ut odio ipsum, tincidunt in tortor vel, semper condimentum leo. Phasellus finibus ac mauris in imperdiet. Praesent auctor enim nec mi luctus cursus. Maecenas ut ipsum at massa elementum tempor quis et massa. Maecenas id nibh eleifend, sollicitudin nunc vel, commodo velit.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet lorem non leo feugiat pulvinar. Nunc commodo posuere arcu, ut consequat ante viverra eu. Integer risus est, sodales sed urna et, pulvinar aliquam sem. Donec mi sem, iaculis eu augue vel, ultricies pretium quam. Ut odio ipsum, tincidunt in tortor vel, semper condimentum leo. Phasellus finibus ac mauris in imperdiet. Praesent auctor enim nec mi luctus cursus. Maecenas ut ipsum at massa elementum tempor quis et massa. Maecenas id nibh eleifend, sollicitudin nunc vel, commodo velit.</p>
-	</div><!--w50-->
-	<div class="w50">
-		<img src="imagens/foto.jpg" />
-	</div><!--w50-->
-</section><!--descricao-autor-->
+	<section class="banne-principal">
+		<div class="center">
+		<form>
+			<h2>Qual o seu melhor e-mail?</h2>
+			<input type="email" name="email" required />
+			<input type="submit" name="acao" value="Cadastrar!" />
+		</form>
+		</div><!--center-->
+	</section><!--banner-principal-->
+	<section class="descricao-autor">
+		<div class="center">
+		<div class="w50">
+			<h2>Henrique de Castro</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet lorem non leo feugiat pulvinar. Nunc commodo posuere arcu, ut consequat ante viverra eu. Integer risus est, sodales sed urna et, pulvinar aliquam sem. Donec mi sem, iaculis eu augue vel, ultricies pretium quam. Ut odio ipsum, tincidunt in tortor vel, semper condimentum leo. Phasellus finibus ac mauris in imperdiet. Praesent auctor enim nec mi luctus cursus. Maecenas ut ipsum at massa elementum tempor quis et massa. Maecenas id nibh eleifend, sollicitudin nunc vel, commodo velit.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet lorem non leo feugiat pulvinar. Nunc commodo posuere arcu, ut consequat ante viverra eu. Integer risus est, sodales sed urna et, pulvinar aliquam sem. Donec mi sem, iaculis eu augue vel, ultricies pretium quam. Ut odio ipsum, tincidunt in tortor vel, semper condimentum leo. Phasellus finibus ac mauris in imperdiet. Praesent auctor enim nec mi luctus cursus. Maecenas ut ipsum at massa elementum tempor quis et massa. Maecenas id nibh eleifend, sollicitudin nunc vel, commodo velit.</p>
+		</div><!--w50-->
+		<div class="w50">
+			<img src="imagens/foto.jpg" />
+		</div><!--w50-->
+		</div><!--center-->
+	</section><!--descricao-autor-->
+	<section class="especialidades">
+		<div class="center">
+		<h2 class="title">Especialidades</h2>
+			<div class="box-especialidade">
+				<h3>Icone1</h3>
+				<h3>CSS3</h3>
+				<p>Nullam luctus tempor mollis. Etiam a finibus nulla. Donec justo purus, tincidunt a finibus dictum, eleifend sit amet velit. Cras auctor lacus eu tellus rhoncus maximus eget et quam. Mauris lobortis sodales sapien tincidunt molestie.</p>
+			</div><!--box-especialidade-->
+			<div class="box-especialidade">
+				<h3>Icone2</h3>
+				<h3>HTML5</h3>
+				<p>Nullam luctus tempor mollis. Etiam a finibus nulla. Donec justo purus, tincidunt a finibus dictum, eleifend sit amet velit. Cras auctor lacus eu tellus rhoncus maximus eget et quam. Mauris lobortis sodales sapien tincidunt molestie.</p>
+			</div><!--box-especialidade-->
+			<div class="box-especialidade">
+				<h3>Icone3</h3>
+				<h3>JavaScript</h3>
+				<p>Nullam luctus tempor mollis. Etiam a finibus nulla. Donec justo purus, tincidunt a finibus dictum, eleifend sit amet velit. Cras auctor lacus eu tellus rhoncus maximus eget et quam. Mauris lobortis sodales sapien tincidunt molestie.</p>
+			</div><!--box-especialidade-->
+		</div><!--center-->
+	</section><!--especialidades-->
+	<section class="extras">
+		<div class="center">
+			<div class="w50">
+				<h2 class="title">Depoimentos</h2>
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis ut nisl vestibulum, interdum nisl a, posuere ipsum. Ut sed dolor sollicitudin, congue augue a, dapibus nunc.</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis ut nisl vestibulum, interdum nisl a, posuere ipsum. Ut sed dolor sollicitudin, congue augue a, dapibus nunc.</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis ut nisl vestibulum, interdum nisl a, posuere ipsum. Ut sed dolor sollicitudin, congue augue a, dapibus nunc.</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->
+			</div><!--w50-->
+			<div class="w50">
+				<h2 class="title">Serviços</h2>
+				<div class="servicos">
+					<ul>
+						<li>Maecenas vitae erat nisi. Vestibulum est turpis, facilisis sed risus a, congue pellentesque dui. Nulla diam orci, placerat et purus a, tincidunt cursus massa. Duis lacinia est tellus, id placerat leo pretium vel.</li>
+						<li>Maecenas vitae erat nisi. Vestibulum est turpis, facilisis sed risus a, congue pellentesque dui. Nulla diam orci, placerat et purus a, tincidunt cursus massa. Duis lacinia est tellus, id placerat leo pretium vel.</li>
+						<li>Maecenas vitae erat nisi. Vestibulum est turpis, facilisis sed risus a, congue pellentesque dui. Nulla diam orci, placerat et purus a, tincidunt cursus massa. Duis lacinia est tellus, id placerat leo pretium vel.</li>
+					</ul>
+				</div><!--servicos-->
+			</div><!--w50-->
+		</div><!--center-->
+	</section><!--extras-->
+	<footer>
+		<div class="center">
+			<p>Todos os direitos reservados</p>
+		</div><!--center-->
+	</footer>
 </body>
 </html>
