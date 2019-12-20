56533eff9dfbadd9982986a43794eeec933debc5
Ricky-Castro <58087833+Ricky-Castro@users.noreply.github.com>
2019-12-20 17:08:23 -0300

    construção de estrutura limpa utilizando as tags do html5

--- index.php
+++ index.php
@@ -11,8 +11,39 @@
 
 <header>
 	<div class="Logo">LogoMarca</div><!--Logo-->
-
+	<nav class="desktop">
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Sobre</a></li>
+			<li><a href="">Serviços</a></li>
+			<li><a href="">Contato</a></li>
+		</ul>
+	</nav>
+	<nav class="mobile">
+	<ul>
+		<li><a href="">Home</a></li>
+		<li><a href="">Sobre</a></li>
+		<li><a href="">Serviços</a></li>
+		<li><a href="">Contato</a></li>
+	</ul>
+	</nav>
 </header>
-
+<section class="banne-principal">
+	<form>
+		<h2>Qual o seu melhor e-mail?</h2>
+		<input type="email" name="email" required />
+		<input type="submit" name="acao" value="Cadastrar!" />
+	</form>
+</section><!--banner-principal-->
+<section class="descricao-autor">
+	<div class="w50">
+		<h2>Henrique de Castro</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet lorem non leo feugiat pulvinar. Nunc commodo posuere arcu, ut consequat ante viverra eu. Integer risus est, sodales sed urna et, pulvinar aliquam sem. Donec mi sem, iaculis eu augue vel, ultricies pretium quam. Ut odio ipsum, tincidunt in tortor vel, semper condimentum leo. Phasellus finibus ac mauris in imperdiet. Praesent auctor enim nec mi luctus cursus. Maecenas ut ipsum at massa elementum tempor quis et massa. Maecenas id nibh eleifend, sollicitudin nunc vel, commodo velit.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet lorem non leo feugiat pulvinar. Nunc commodo posuere arcu, ut consequat ante viverra eu. Integer risus est, sodales sed urna et, pulvinar aliquam sem. Donec mi sem, iaculis eu augue vel, ultricies pretium quam. Ut odio ipsum, tincidunt in tortor vel, semper condimentum leo. Phasellus finibus ac mauris in imperdiet. Praesent auctor enim nec mi luctus cursus. Maecenas ut ipsum at massa elementum tempor quis et massa. Maecenas id nibh eleifend, sollicitudin nunc vel, commodo velit.</p>
+	</div><!--w50-->
+	<div class="w50">
+		<img src="imagens/foto.jpg" />
+	</div><!--w50-->
+</section><!--descricao-autor-->
 </body>
 </html>
