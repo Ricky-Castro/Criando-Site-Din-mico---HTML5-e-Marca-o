3475eeeb99e691c6f85e6a7bf88263dbe776034e
Ricky-Castro <58087833+Ricky-Castro@users.noreply.github.com>
2019-12-20 16:45:52 -0300

    Inclusão das tags meta viewport, keywords e description

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto 01</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+	<meta name="keywords" content="Palavras, Separadas, Por, Vírgulas." />
+	<meta name="description" content="Descrição do Meu WebSite." />
+	<meta charset="utf-8">
+</head>
+<body>
+
+<header>
+	<div class="Logo">LogoMarca</div><!--Logo-->
+
+</header>
+
+</body>
+</html>
