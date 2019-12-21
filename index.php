39f8dd96c5dad089b2394c48406e4d9f6ae140b2
Henrique <decastrohenrique@yahoo.com.br>
2019-12-21 18:55:45 -0300

    adicionando estilos com CSS3

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Projeto 01</title>
+	<link rel="stylesheet" href="estilo/font-awesome.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo/style.css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
@@ -13,8 +14,8 @@
 
 <header>
 	<div class="center">
-		<div class="Logo">LogoMarca</div><!--Logo-->
-		<nav class="desktop">
+		<div class="Logo left">LogoMarca</div><!--Logo-->
+		<nav class="desktop right">
 			<ul>
 				<li><a href="">Home</a></li>
 				<li><a href="">Sobre</a></li>
@@ -22,7 +23,7 @@
 				<li><a href="">Contato</a></li>
 			</ul>
 		</nav>
-		<nav class="mobile">
+		<nav class="mobile right">
 			<ul>
 				<li><a href="">Home</a></li>
 				<li><a href="">Sobre</a></li>
@@ -30,6 +31,7 @@
 				<li><a href="">Contato</a></li>
 			</ul>
 		</nav>
+		<div class="clear"></div>
 	</div><!--center-->
 </header>
 	<section class="banne-principal">
@@ -43,39 +45,40 @@
 	</section><!--banner-principal-->
 	<section class="descricao-autor">
 		<div class="center">
-		<div class="w50">
+		<div class="w50 left">
 			<h2>Henrique de Castro</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet lorem non leo feugiat pulvinar. Nunc commodo posuere arcu, ut consequat ante viverra eu. Integer risus est, sodales sed urna et, pulvinar aliquam sem. Donec mi sem, iaculis eu augue vel, ultricies pretium quam. Ut odio ipsum, tincidunt in tortor vel, semper condimentum leo. Phasellus finibus ac mauris in imperdiet. Praesent auctor enim nec mi luctus cursus. Maecenas ut ipsum at massa elementum tempor quis et massa. Maecenas id nibh eleifend, sollicitudin nunc vel, commodo velit.</p>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet lorem non leo feugiat pulvinar. Nunc commodo posuere arcu, ut consequat ante viverra eu. Integer risus est, sodales sed urna et, pulvinar aliquam sem. Donec mi sem, iaculis eu augue vel, ultricies pretium quam. Ut odio ipsum, tincidunt in tortor vel, semper condimentum leo. Phasellus finibus ac mauris in imperdiet. Praesent auctor enim nec mi luctus cursus. Maecenas ut ipsum at massa elementum tempor quis et massa. Maecenas id nibh eleifend, sollicitudin nunc vel, commodo velit.</p>
-		</div><!--w50-->
-		<div class="w50">
-			<img src="imagens/foto.jpg" />
-		</div><!--w50-->
+		</div><!--w50 left-->
+		<div class="w50 left">
+			<img src="imagens/foto.jpg" width="300" height="300" />
+		</div><!--w50 left-->
+		<div class="clear"></div>
 		</div><!--center-->
 	</section><!--descricao-autor-->
 	<section class="especialidades">
 		<div class="center">
 		<h2 class="title">Especialidades</h2>
-			<div class="box-especialidade">
-				<h3>Icone1</h3>
+			<div class="w33 left box-especialidade">
+				<h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
 				<h3>CSS3</h3>
 				<p>Nullam luctus tempor mollis. Etiam a finibus nulla. Donec justo purus, tincidunt a finibus dictum, eleifend sit amet velit. Cras auctor lacus eu tellus rhoncus maximus eget et quam. Mauris lobortis sodales sapien tincidunt molestie.</p>
-			</div><!--box-especialidade-->
-			<div class="box-especialidade">
-				<h3>Icone2</h3>
+			</div><!--w33 left box-especialidade-->
+			<div class="w33 left box-especialidade">
+				<h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
 				<h3>HTML5</h3>
 				<p>Nullam luctus tempor mollis. Etiam a finibus nulla. Donec justo purus, tincidunt a finibus dictum, eleifend sit amet velit. Cras auctor lacus eu tellus rhoncus maximus eget et quam. Mauris lobortis sodales sapien tincidunt molestie.</p>
-			</div><!--box-especialidade-->
-			<div class="box-especialidade">
+			</div><!--w33 left box-especialidade-->
+			<div class="w33 left box-especialidade">
 				<h3>Icone3</h3>
 				<h3>JavaScript</h3>
 				<p>Nullam luctus tempor mollis. Etiam a finibus nulla. Donec justo purus, tincidunt a finibus dictum, eleifend sit amet velit. Cras auctor lacus eu tellus rhoncus maximus eget et quam. Mauris lobortis sodales sapien tincidunt molestie.</p>
-			</div><!--box-especialidade-->
+			</div><!--w33 left box-especialidade-->
 		</div><!--center-->
 	</section><!--especialidades-->
 	<section class="extras">
 		<div class="center">
-			<div class="w50">
+			<div class="w50 left">
 				<h2 class="title">Depoimentos</h2>
 				<div class="depoimento-single">
 					<p class="depoimento-descricao">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis ut nisl vestibulum, interdum nisl a, posuere ipsum. Ut sed dolor sollicitudin, congue augue a, dapibus nunc.</p>
@@ -89,8 +92,8 @@
 					<p class="depoimento-descricao">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis ut nisl vestibulum, interdum nisl a, posuere ipsum. Ut sed dolor sollicitudin, congue augue a, dapibus nunc.</p>
 					<p class="nome-autor">Lorem Ipsum</p>
 				</div><!--depoimento-single-->
-			</div><!--w50-->
-			<div class="w50">
+			</div><!--w50 left-->
+			<div class="w50 left">
 				<h2 class="title">Serviços</h2>
 				<div class="servicos">
 					<ul>
@@ -99,7 +102,8 @@
 						<li>Maecenas vitae erat nisi. Vestibulum est turpis, facilisis sed risus a, congue pellentesque dui. Nulla diam orci, placerat et purus a, tincidunt cursus massa. Duis lacinia est tellus, id placerat leo pretium vel.</li>
 					</ul>
 				</div><!--servicos-->
-			</div><!--w50-->
+			</div><!--w50 left-->
+			<div class="clear"></div>
 		</div><!--center-->
 	</section><!--extras-->
 	<footer>
