85f2672ed492bd521d1acaecd05c3eb160a3053c
Henrique de Castro <decastrohenrique@yahoo.com.br>
2019-12-22 17:40:24 -0300

    estilizando formulário

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Projeto 01</title>
 	<link rel="stylesheet" href="estilo/font-awesome.min.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="estilo/style.css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 	<meta name="keywords" content="Palavras, Separadas, Por, Vírgulas." />
@@ -14,7 +14,7 @@
 
 <header>
 	<div class="center">
-		<div class="Logo left">LogoMarca</div><!--Logo-->
+		<div class="logo left">LogoMarca</div><!--Logo-->
 		<nav class="desktop right">
 			<ul>
 				<li><a href="">Home</a></li>
@@ -34,7 +34,8 @@
 		<div class="clear"></div>
 	</div><!--center-->
 </header>
-	<section class="banne-principal">
+	<section class="banner-principal">
+		<div class="overlay"></div><!--overlay-->
 		<div class="center">
 		<form>
 			<h2>Qual o seu melhor e-mail?</h2>
@@ -51,7 +52,7 @@
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet lorem non leo feugiat pulvinar. Nunc commodo posuere arcu, ut consequat ante viverra eu. Integer risus est, sodales sed urna et, pulvinar aliquam sem. Donec mi sem, iaculis eu augue vel, ultricies pretium quam. Ut odio ipsum, tincidunt in tortor vel, semper condimentum leo. Phasellus finibus ac mauris in imperdiet. Praesent auctor enim nec mi luctus cursus. Maecenas ut ipsum at massa elementum tempor quis et massa. Maecenas id nibh eleifend, sollicitudin nunc vel, commodo velit.</p>
 		</div><!--w50 left-->
 		<div class="w50 left">
-			<img src="imagens/foto.jpg" width="300" height="300" />
+			<img src="imagens/foto.jpg" width="400" height="400" />
 		</div><!--w50 left-->
 		<div class="clear"></div>
 		</div><!--center-->
