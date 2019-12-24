b77ac0e0002197c10de09b3206ba4d1232fb2bd7
Henrique de Castro <decastrohenrique@yahoo.com.br>
2019-12-23 21:39:59 -0300

    Feito a validação com php, definido o estilo da pagina de erro, usando o header e o footer padrão para todas as páginas.

--- index.php
+++ index.php
@@ -38,81 +38,19 @@
 		<div class="clear"></div>
 	</div><!--center-->
 </header>
-	<section class="banner-principal">
-		<div class="overlay"></div><!--overlay-->
-		<div class="center">
-		<form>
-			<h2>Qual o seu melhor e-mail?</h2>
-			<input type="email" name="email" required />
-			<input type="submit" name="acao" value="Cadastrar!" />
-		</form>
-		</div><!--center-->
-	</section><!--banner-principal-->
-	<section class="descricao-autor">
-		<div class="center">
-		<div class="w50 left">
-			<h2>Henrique de Castro</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet lorem non leo feugiat pulvinar. Nunc commodo posuere arcu, ut consequat ante viverra eu. Integer risus est, sodales sed urna et, pulvinar aliquam sem. Donec mi sem, iaculis eu augue vel, ultricies pretium quam. Ut odio ipsum, tincidunt in tortor vel, semper condimentum leo. Phasellus finibus ac mauris in imperdiet. Praesent auctor enim nec mi luctus cursus. Maecenas ut ipsum at massa elementum tempor quis et massa. Maecenas id nibh eleifend, sollicitudin nunc vel, commodo velit.</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet lorem non leo feugiat pulvinar. Nunc commodo posuere arcu, ut consequat ante viverra eu. Integer risus est, sodales sed urna et, pulvinar aliquam sem. Donec mi sem, iaculis eu augue vel, ultricies pretium quam. Ut odio ipsum, tincidunt in tortor vel, semper condimentum leo. Phasellus finibus ac mauris in imperdiet. Praesent auctor enim nec mi luctus cursus. Maecenas ut ipsum at massa elementum tempor quis et massa. Maecenas id nibh eleifend, sollicitudin nunc vel, commodo velit.</p>
-		</div><!--w50 left-->
-		<div class="w50 left">
-			<img class="right" src="<?php echo INCLUDE_PATH; ?>imagens/foto.jpg" />
-		</div><!--w50 left-->
-		<div class="clear"></div>
-		</div><!--center-->
-	</section><!--descricao-autor-->
-	<section class="especialidades">
-		<div class="center">
-		<h2 class="title">Especialidades</h2>
-			<div class="w33 left box-especialidade">
-				<h3><i class="fab fa-css3-alt"></i></h3>
-				<h4>CSS3</h4>
-				<p>Nullam luctus tempor mollis. Etiam a finibus nulla. Donec justo purus, tincidunt a finibus dictum, eleifend sit amet velit. Cras auctor lacus eu tellus rhoncus maximus eget et quam. Mauris lobortis sodales sapien tincidunt molestie.</p>
-			</div><!--w33 left box-especialidade-->
-			<div class="w33 left box-especialidade">
-				<h3><i class="fab fa-html5"></i></h3>
-				<h4>HTML5</h4>
-				<p>Nullam luctus tempor mollis. Etiam a finibus nulla. Donec justo purus, tincidunt a finibus dictum, eleifend sit amet velit. Cras auctor lacus eu tellus rhoncus maximus eget et quam. Mauris lobortis sodales sapien tincidunt molestie.</p>
-			</div><!--w33 left box-especialidade-->
-			<div class="w33 left box-especialidade">
-				<h3><i class="fab fa-js-square"></i></h3>
-				<h4>JavaScript</h4>
-				<p>Nullam luctus tempor mollis. Etiam a finibus nulla. Donec justo purus, tincidunt a finibus dictum, eleifend sit amet velit. Cras auctor lacus eu tellus rhoncus maximus eget et quam. Mauris lobortis sodales sapien tincidunt molestie.</p>
-			</div><!--w33 left box-especialidade-->
-			<div class="clear"></div><!--clear-->
-		</div><!--center-->
-	</section><!--especialidades-->
-	<section class="extras">
-		<div class="center">
-			<div class="w50 left depoimento-container">
-				<h2 class="title">Depoimentos dos nossos clientes</h2>
-				<div class="depoimento-single">
-					<p class="depoimento-descricao">"Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis ut nisl vestibulum, interdum nisl a, posuere ipsum. Ut sed dolor sollicitudin, congue augue a, dapibus nunc."</p>
-					<p class="nome-autor">Lorem Ipsum</p>
-				</div><!--depoimento-single-->
-				<div class="depoimento-single">
-					<p class="depoimento-descricao">"Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis ut nisl vestibulum, interdum nisl a, posuere ipsum. Ut sed dolor sollicitudin, congue augue a, dapibus nunc."</p>
-					<p class="nome-autor">Lorem Ipsum</p>
-				</div><!--depoimento-single-->
-				<div class="depoimento-single">
-					<p class="depoimento-descricao">"Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis ut nisl vestibulum, interdum nisl a, posuere ipsum. Ut sed dolor sollicitudin, congue augue a, dapibus nunc."</p>
-					<p class="nome-autor">Lorem Ipsum</p>
-				</div><!--depoimento-single-->
-			</div><!--w50 left-->
-			<div class="w50 left servicos-container">
-				<h2 class="title">Serviços</h2>
-				<div class="servicos">
-					<ul>
-						<li>Maecenas vitae erat nisi. Vestibulum est turpis, facilisis sed risus a, congue pellentesque dui. Nulla diam orci, placerat et purus a, tincidunt cursus massa. Duis lacinia est tellus, id placerat leo pretium vel.</li>
-						<li>Maecenas vitae erat nisi. Vestibulum est turpis, facilisis sed risus a, congue pellentesque dui. Nulla diam orci, placerat et purus a, tincidunt cursus massa. Duis lacinia est tellus, id placerat leo pretium vel.</li>
-						<li>Maecenas vitae erat nisi. Vestibulum est turpis, facilisis sed risus a, congue pellentesque dui. Nulla diam orci, placerat et purus a, tincidunt cursus massa. Duis lacinia est tellus, id placerat leo pretium vel.</li>
-					</ul>
-				</div><!--servicos-->
-			</div><!--w50 left-->
-			<div class="clear"></div>
-		</div><!--center-->
-	</section><!--extras-->
-	<footer>
+	<?php 
+
+		$url = isset($_GET['url']) ? $_GET['url'] : 'home';
+		if(file_exists('pages/'.$url.'.php')){
+			include('pages/'.$url.'.php');
+		}
+		else{
+			$pagina404 = true;
+			include('pages/404.php');
+		}
+
+	 ?>
+	<footer <?php if(isset($pagina404) && $pagina404 == true) echo 'class="fixed"'; ?>>
 		<div class="center">
 			<p>Todos os direitos reservados</p>
 		</div><!--center-->
---
+++ pages/404.php
@@ -0,0 +1,8 @@
+<section class="erro-404">
+	<div class="center">
+		<div class="wraper-404">
+		<h2><i style="padding: 10px;" class="fas fa-times"></i>A página não existe!</h2>
+		<p>Voltar para a <a href="<?php echo INCLUDE_PATH; ?>">página princial</a></p>
+		</div><!--wraper-404-->
+	</div><!--center-->
+</section>
